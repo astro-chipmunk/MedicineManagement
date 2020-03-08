1379db7a5782d771f37cde767d8dc0f26e37dccb
幸泉花梨 <koizumikarin@MacBook-Pro.local>
2020-03-08 22:11:23 +0900

    v3

--- app/src/main/java/com/koizumikarin/medimanage/RetrofitMaps.java
+++ app/src/main/java/com/koizumikarin/medimanage/RetrofitMaps.java
@@ -13,7 +13,6 @@ public interface RetrofitMaps {
      * Retrofit get annotation with our URL
      * And our method that will return us details of student.
      */
-    @GET("api/place/nearbysearch/json?sensor=true&key=AIzaSyDN7RJFmImYAca96elyZlE5s_fhX-MMuhk")
+    @GET("api/place/nearbysearch/json?sensor=true&key=AIzaSyARFjZEcY-IJ0J0wbFPZ4LdPfG0AHs-Zk4")
     Call<Example> getNearbyPlaces(@Query("type") String type, @Query("location") String location, @Query("radius") int radius);
-
 }
